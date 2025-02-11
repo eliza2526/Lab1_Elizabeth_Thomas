68b67c80b6da25b1f45d7159fda6a85fb3757772
Elizabeth Thomas <157935947+eliza2526@users.noreply.github.com>
2025-02-11 15:25:40 -0500

    isPrime working
    Text Input for random number created
    Prime, NotPrime Button added
    IsPrime function working - checkmark if prime, xmark if not prime

---
+++ Lab1_Elizabeth_Thomas/Lab1_Elizabeth_Thomas/Lab1_Elizabeth_Thomas/Lab1_Elizabeth_ThomasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Elizabeth_ThomasApp.swift
+//  Lab1_Elizabeth_Thomas
+//
+//  Created by Elizabeth Thomas on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Elizabeth_ThomasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
